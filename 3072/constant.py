f12a824190a3a7b6eeedf6486e34e298748aede4
Raizel119 <richardlcp119@gmail.com>
2023-12-06 00:11:33 +0700

    change coding

--- 3072/constant.py
+++ 3072/constant.py
@@ -1,20 +1,33 @@
+# ukuran panjang dan lebar
 SIZE = 400
 
 # panjang x lebar grid
 GRID_LEN = 4
 
 # spasi antar cell
-GRID_PADDING = 10
+PADDING = 10
+
+# teks score
+SCORE_LABEL_FONT = ("Verdana", 20)
+SCORE_FONT = ("Helvetica", 32, "bold")
+
+# warna bg game
+GRID_COLOR = "#505160"
 
 # warna cell kosong
-BG_COLOR_GAME = "#92877d"
-BG_COLOR_EMPTY = "#9e948a"
+EMPTY_CELL_BG = "#68829E"
+
+# warna dan font untuk tampilan sistem
+STATE_FONT = ("Helvetica", 48, "bold")
+STATE_FONT_COLOR = "#ffffff"
+WINNER_BG = "#ffcc00"
+LOSER_BG = "#a39489"
 
 # warna background angka
-BG_COLOR_DICT = {
+BG_COLOR = {
 3:      "#eee4da",
 6:      "#ede0c8",
-12:      "#f2b179",
+12:     "#f2b179",
 24:     "#f59563",
 48:     "#f67c5f",
 96:     "#f65e3b",
@@ -22,19 +35,14 @@ BG_COLOR_DICT = {
 384:    "#edcc61",
 768:    "#edc850",
 1536:   "#edc53f",
-3072:   "#edc22e",
-6144:   "#eee4da",
-12288:   "#edc22e",
-24576:  "#f2b179",
-49152:  "#f59563",
-98304:  "#f67c5f",
+3072:   "#edc22e"
 }
 
 # warna angka
-NUM_COLOR_DICT = {
+NUMBER_COLOR = {
 3:      "#776e65",
 6:      "#776e65",
-12:      "#f9f6f2",
+12:     "#f9f6f2",
 24:     "#f9f6f2",
 48:     "#f9f6f2",
 96:     "#f9f6f2",
@@ -42,30 +50,20 @@ NUM_COLOR_DICT = {
 384:    "#f9f6f2",
 768:    "#f9f6f2",
 1536:   "#f9f6f2",
-3072:   "#f9f6f2",
-6144:   "#776e65",
-12288:   "#f9f6f2",
-24576:  "#776e65",
-49152:  "#776e65",
-98304:  "#f9f6f2",
+3072:   "#f9f6f2"
 }
 
-FONT = ("Verdana",40,"bold")
-
-KEY_QUIT = "Escape"
-KEY_BACK = "b"
-
-KEY_UP = "Up"
-KEY_DOWN = "Down"
-KEY_LEFT = "Left"
-KEY_RIGHT = "Right"
-
-KEY_UP_ALT1 = "w"
-KEY_DOWN_ALT1 = "s"
-KEY_LEFT_ALT1 = "a"
-KEY_RIGHT_ALT1 = "d"
-
-KEY_UP_ALT2 = "i"
-KEY_DOWN_ALT2 = "k"
-KEY_LEFT_ALT2 = "j"
-KEY_RIGHT_ALT2 = "l"
+# font angka
+NUMBER_FONTS = {
+3:      ("Helvetica", 55, "bold"),
+6:      ("Helvetica", 55, "bold"),
+12:     ("Helvetica", 55, "bold"),
+24:     ("Helvetica", 50, "bold"),
+48:     ("Helvetica", 50, "bold"),
+96:     ("Helvetica", 50, "bold"),
+192:    ("Helvetica", 45, "bold"),
+384:    ("Helvetica", 45, "bold"),
+768:    ("Helvetica", 45, "bold"),
+1536:   ("Helvetica", 40, "bold"),
+3072:   ("Helvetica", 40, "bold")
+}
